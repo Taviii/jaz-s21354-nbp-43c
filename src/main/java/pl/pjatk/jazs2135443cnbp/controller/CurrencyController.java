3d8ccd2a7a09eb995d6e5c833296189c11bb9ef8
Tavi <56189672+s21354-pj@users.noreply.github.com>
2021-07-04 21:11:50 +0200

    Add files via upload
    AKTUALNIEJSZE!

--- src/main/java/pl/pjatk/jazs2135443cnbp/controller/CurrencyController.java
+++ src/main/java/pl/pjatk/jazs2135443cnbp/controller/CurrencyController.java
@@ -1,5 +1,7 @@
 package pl.pjatk.jazs2135443cnbp.controller;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.pjatk.jazs2135443cnbp.service.CurrencyService;
@@ -16,6 +18,12 @@ public class CurrencyController {
     }
 
     @GetMapping("/tables")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")})
     public ResponseEntity<Void> getTables(@PathVariable String startDate,
                                           @PathVariable String endDate) {
         currencyService.getTables(startDate, endDate);
